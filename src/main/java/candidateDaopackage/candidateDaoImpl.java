ba9cc17963630eefa67b41c71d16bf645845610b
skar8000 <52293100+skar8000@users.noreply.github.com>
2019-07-12 14:19:18 +0530

    candidate
    this contains candidate dao package and
    the changes in candidate

---
+++ src/main/java/candidateDaopackage/candidateDaoImpl.java
@@ -0,0 +1,47 @@
+package candidateDaopackage;
+
+import model.candidate;
+import model.education;
+
+import model.experience;
+
+public class candidateDaoImpl implements candidateDao {
+
+	public void createCandidate(candidate c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateCandidate(candidate c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addExperience(String candidateMobileNo, experience ex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addEducation(String candidateMobileNo, education ex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateExperience(String candidateMobileNo, experience ex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateEducation(String candidateMobileNo, education ex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteCandidate(String candidateMobileNo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
